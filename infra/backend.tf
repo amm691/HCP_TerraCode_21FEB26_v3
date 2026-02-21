2e8ee6b38b767d66b62cdd44ec6bcd372bd69628
Amit <amit@email.com>
2026-02-21 16:16:45 +0530

    1st commit

---
+++ infra/backend.tf
@@ -0,0 +1,6 @@
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-bucket-amt-21feb26"
+    prefix  = "gke-3apps"
+  }
---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+infra/variables.tf
+variable "project_id" {}
+variable "region" {
+  default = "asia-south1"
+}
